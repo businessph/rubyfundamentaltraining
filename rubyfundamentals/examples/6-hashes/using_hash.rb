dba499211b820920e5bbc47fcebd6ad77856ede7
phuong hua <businessph>
2015-09-23 16:28:19 -0700

    09232015 1628

--- rubyfundamentals/examples/6-hashes/using_hash.rb
+++ rubyfundamentals/examples/6-hashes/using_hash.rb
@@ -5,12 +5,12 @@ hash1 = {	'Robert'=>'54 Main st, SF 415-999-8765',
 	        'Carl'=>'567 Market St, SF 415-223-4588',
           'Ivan'=>'444 5th St, Oakland 510-986-3343' }
 
-
 ###random key cla ss
 class Key
 
 end
 
+### create a new key
 key = Key.new
 
 ### harder way to create hashmap, make hash2 equal to a new hash
@@ -26,27 +26,36 @@ hash2['videographer'] = 'camera'
 ### hash 4 the key is set to a new key
 hash2[key] = 'Ballerina'
 
+### code starts here ###
+
 puts "\nhash2.1  "
 puts hash2
 
-puts "\n***A.  " + hash1['Ivan']	
+puts "\n***A.  " + hash1['Robert']   ### in hash1 you have key Robert, return Robert value
 
 print "\n***B.  " 
-puts hash1['Alex'] #returns nil bc there is no alex
+puts hash1['Alex'] ###returns blank nil bc there is no alex
 
-puts "\n***C.  " + hash2['doctor'] 
+puts "\n***C.  " + hash2['doctor']   ### in hash2 you have key doctor, return doctor value
 
-puts "\n***D.  " + hash2[key] 
+puts "\n***D.  " + hash2[key]        ### in hash2 you have key key, return key value
 
-puts "\n***E.  " + hash2['dancer']	 # returns default value ('Some kind of tool')
+puts "\n***E.  " + hash2['dancer']	 ### there is no key dancer in hash2, returns default value ('Some kind of tool') if there is no key in the hash2
 
 print "\n***F.  " 
-puts hash1.default		#=> nil      ### there is no default
+puts hash1.default		### => nil     ### there is no default for hash1
 
-puts "\n***G.  " + hash2.default				### hash2 = Hash.new("Some kind of tool")   ###assigns the default
+puts "\n***G.  " + hash2.default				### hash2 = Hash.new("Some kind of tool")   ###hash2.default gives the value of
 
-hash1.default = 'Somewhere in the US'  ### hash1 default is set
+hash1.default = 'Somewhere in the US'  ### hash1 is now given a default value
 
-puts "\n***H.  " + hash1["Max"]	
-		
-				
+puts "\n***H.  " + hash1["Max"]        ### hash1 is now given a default value, there is no Max key, a default value will be generated
+
+
+### set default value for hashmap, wh en user requests nil key default value will be displayed
+###
+### hash1.default = 'there is no key value in the hash1'
+###
+### and
+###
+### hash1 = Hash.new("there is no key value in the hash1")
