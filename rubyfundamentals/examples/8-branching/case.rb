8694d694a965a1329e0aabfce3da2b6d0117322a
phuong hua <businessph>
2015-09-24 17:04:08 -0700

    09242015 1704 branching

--- rubyfundamentals/examples/8-branching/case.rb
+++ rubyfundamentals/examples/8-branching/case.rb
@@ -1,8 +1,9 @@
 # case...when...else
 
-def showDay( day_number )
-	case( day_number ) 
-		when 1,'Monday','Mon' then puts "It's Monday" 
+def showDay(day_number)
+	case(day_number)
+		when 1, '1', 'Monday', 'Mon', 'monday', 'mon' then puts "It's Monday"
+		# when 1, '1', 'Monday', 'Mon', 'monday', 'mon' then puts "It's Monday"   ###when 1, Monday or Mon puts it's monday
 		when 2 then puts "It's Tuesday" 
 		when 3 then puts "It's Wednesday" 
 		when 4 then puts "It's Thursday" 
@@ -10,11 +11,22 @@ def showDay( day_number )
 		             puts "It's almost the weekend" 
 		when 6,7 
                  puts "It's the weekend!!! " 
-		else puts "Number #{day_number} is not a valid day!" 
+		else puts "Number #{day_number} is not a valid day!"
 	end
 end
 
+print('what day is it? ')
+ask_day = gets.chomp
+showDay(ask_day)
 
+
+puts
 for whichDay in 1..8 do
-	showDay( whichDay )
+	showDay(whichDay)
 end
+
+
+
+# "hello James!".downcase    #=> "hello james!"
+# "hello James!".upcase      #=> "HELLO JAMES!"
+# "hello James!".capitalize  #=> "Hello james!"
