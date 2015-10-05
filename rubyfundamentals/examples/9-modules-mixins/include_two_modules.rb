164000e6627f1f10e3f44e419e7f1dedbdcf5830
phuong hua <businessph>
2015-10-05 10:11:37 -0700

    10052015 0808 monday1

--- rubyfundamentals/examples/9-modules-mixins/include_two_modules.rb
+++ rubyfundamentals/examples/9-modules-mixins/include_two_modules.rb
@@ -1,40 +1,45 @@
-# modules included in subclass
+### modules included in subclass
+
+PH_TAX_PERCENT = 0.55 ###Global variable
 
 module Tax
-  TAX_PERCENT = 0.35
-  
-  attr_accessor :payment_amount
-	
+  TAX_PERCENT = 0.35 ###global variable of module
+  attr_accessor :payment_amount ### set and get
 end
 
 module Transportation
-	attr_accessor :time, :cost
+	attr_accessor :time, :cost ### set and get
 end
 
 class Vehicle
-	attr_accessor :color, :description
+	attr_accessor :color, :description ### set and get
 end
 
-class Car < Vehicle
-  include Tax
-  include Transportation
-  
-  attr_accessor :transmission
+class Car < Vehicle ### inherits super Vehicle class into sub Car class
+  include Tax ### include module
+  include Transportation  ### include module
+  attr_accessor :transmission ### set and get
 end
 
-car1 = Car.new
-car1.transmission = "manual"
-car1.color = "red"
-car1.description = "restored classic"
-car1.payment_amount = 550.99
-
-car1.time = 4
-car1.cost=125
+### what is available in Car class, tax_percent, payment_amount, time, cost, color, description, and transmission
 
 
-printf "\nGrand total: $%.2f",car1.payment_amount + 
-                              car1.payment_amount * Tax::TAX_PERCENT + 
-                              car1.time*car1.cost
+car1 = Car.new ###create new instance of car class
+
+car1.transmission = "manual" ### set
+car1.color = "red" ### set
+car1.description = "restored classic" ### set
+car1.payment_amount = 550.99 ### set
+car1.time = 4 ### set
+car1.cost=125 ### set
+
+printf "\nGrand total: $%.2f",car1.payment_amount +                    ### payment_amount plus
+                              car1.payment_amount * Tax::TAX_PERCENT + ### payment_amount times Tax module::Global variable TAX_PERCENT plus
+                              car1.time * car1.cost                    ### time times cost = total
+
+printf "\nGrand total: $%.2f",car1.payment_amount +                  ### payment_amount plus
+                              car1.payment_amount * PH_TAX_PERCENT + ### payment_amount times Tax module::Global variable TAX_PERCENT plus
+                              car1.time * car1.cost                  ### time times cost = total
 
 puts "\n\n"
 
