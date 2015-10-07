d03d06a7b582cb779844c97a17ee0259e6070844
phuong hua <businessph>
2015-10-06 17:39:44 -0700

    1006

--- rubyfundamentals/examples/9-modules-mixins/include_two_modules.rb
+++ rubyfundamentals/examples/9-modules-mixins/include_two_modules.rb
@@ -1,6 +1,6 @@
 ### modules included in subclass
 
-PH_TAX_PERCENT = 0.55 ###Global variable
+PH_TAX_PERCENT = 0.925 ###Global variable
 
 module Tax
   TAX_PERCENT = 0.35 ###global variable of module
@@ -26,20 +26,18 @@ end
 
 car1 = Car.new ###create new instance of car class
 
-car1.transmission = "manual" ### set
-car1.color = "red" ### set
+car1.transmission = "manual"          ### set
+car1.color = "red"                    ### set
 car1.description = "restored classic" ### set
-car1.payment_amount = 550.99 ### set
-car1.time = 4 ### set
-car1.cost=125 ### set
+car1.payment_amount = 550.99          ### set
+car1.time = 4                         ### set
+car1.cost = 125                       ### set
 
 printf "\nGrand total: $%.2f",car1.payment_amount +                    ### payment_amount plus
                               car1.payment_amount * Tax::TAX_PERCENT + ### payment_amount times Tax module::Global variable TAX_PERCENT plus
                               car1.time * car1.cost                    ### time times cost = total
 
-printf "\nGrand total: $%.2f",car1.payment_amount +                  ### payment_amount plus
-                              car1.payment_amount * PH_TAX_PERCENT + ### payment_amount times Tax module::Global variable TAX_PERCENT plus
-                              car1.time * car1.cost                  ### time times cost = total
+printf "\nGrand total: $%.20f",car1.payment_amount + (car1.payment_amount * PH_TAX_PERCENT) + (car1.time * car1.cost)
 
 puts "\n\n"
 
