19e3b66c23f85b6cb4e07ec0456a52f72efc7bc8
phuong hua <businessph>
2015-10-14 11:55:00 -0700

    10142015 1154

--- rubyfundamentals/examples/9-modules-mixins/module.rb
+++ rubyfundamentals/examples/9-modules-mixins/module.rb
@@ -2,8 +2,8 @@
 #no access to the give_opinion method
 
 module MoodModule
-   GOOD_MOOD = "optimistic"
-   BAD_MOOD = "pessimistic"
+   GOOD_MOOD  = "optimistic"
+   BAD_MOOD   = "pessimistic"
    
 	def give_opinion
 		return "I'm #{GOOD_MOOD}. I love ruby"
@@ -14,11 +14,10 @@ module MoodModule
 	end
 end	
 
-
-puts "MoodModule::GOOD_MOOD"
-puts "\t" + MoodModule::GOOD_MOOD
-puts "MoodModule.give_opinion" 
-puts "\t" + MoodModule.give_opinion 
+puts "\nMoodModule::GOOD_MOOD"        ###prints what is in double quotes
+puts "\n\t" + MoodModule::GOOD_MOOD   ### prints module value
+puts "\nMoodModule.give_opinion"      ###prints what is in double quotes
+puts "\n\t" + MoodModule.give_opinion ### prints module value n method
 
 #puts give_opinion
 
