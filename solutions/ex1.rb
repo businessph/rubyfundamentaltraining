f0967450ffd6f916ae1fa1e72843e5788fe36a1e
phuong hua <businessph>
2015-10-26 15:57:26 -0700

    ex1 update with descriptions, refactor code

--- solutions/ex1.rb
+++ solutions/ex1.rb
@@ -1,28 +1,37 @@
- def getUserInfo
-    
-    print "Enter product name: " 
-    productName = gets.chomp
 
-    print "Enter desired weight as adecimal number:   "
-    desiredWeight = gets.to_f.round(2)  
-    
-    cost = desiredWeight * 4.5 #cost per pound
-    
-    print "Yor ordered #{desiredWeight} of #{productName} for the cost of "
-    printf("%.2f" , cost)  
-     
-    puts "\n" + updateOrder(cost)
-    
-    #puts "\n" + (updateOrder cost) - parenthesis needed for precedance surronding the method call or the parameter
-    
-    
+### getUserInfo
+def getUserInfo
+
+  ### desiredWeight
+  print "Enter desired weight as a decimal number:   "
+  desiredWeight = gets.to_f.round(2)
+
+  ### productName
+  print "Enter product name: "
+  productName = gets.chomp
+
+  ### cost
+  cost = desiredWeight * 4.5 #cost per pound
+
+  ### print line
+  print "Yor ordered #{desiredWeight} of #{productName} for the cost of "
+  print cost.to_f.round(2)
+
+  ###These are the same
+  ### printf("%.2f", cost)
+  ### print cost.to_f.round(2)
+
+  ### updateOrder, external method, puts method that gets returned
+  puts "\n" + updateOrder(cost)
+  #puts "\n" + (updateOrder cost) - parenthesis needed for precedence surrounding the method call or the parameter
+
   end
-  
-  def updateOrder(theCost) 
-    return "Thanks for ordering - the revised cost is $#{theCost*10}" 
-  end
-  
 
+###string from getUserInfo gets put into this method and multiplies it by ten
+def updateOrder(theCost)
+  return "Thanks for ordering - the revised cost is $#{theCost*10}"
+end
 
-  getUserInfo
+### run first method
+getUserInfo
 
