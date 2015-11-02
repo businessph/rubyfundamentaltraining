4c05e6f3b1133b1921acba587d330d43cbf2ae0d
phuong hua <businessph>
2015-11-02 14:43:23 -0800

    11022015 1443 updated ex6

--- solutions/ex6.rb
+++ solutions/ex6.rb
@@ -1,31 +1,36 @@
-employeeCitiesHash1 = {  
+
+employeeCitiesHash1 = {
   'Racel'=>'San Francisco',
   'Brian'=>'Oakland',
   'Sarah'=>'Berkely',
   'Joseph'=>'Palo Alto',
-  }
-  
-  
-puts "*** Gets value using a key ***"
-puts employeeCitiesHash1['Brian']     
+                        }
+puts "\n1*** Gets value using a key ***"
+puts employeeCitiesHash1['Racel']
+puts employeeCitiesHash1['Racel1']
 
 employeeCitiesHash1.default = "Bay Area"
+puts "\n2*** Gets value using a non-existing key so it gets the default***"
+puts employeeCitiesHash1['Ruven']
+puts employeeCitiesHash1['Racel1']
 
-puts "\n*** Gets value using a non-existing key so it gets the default***"
-puts employeeCitiesHash1['Ruven'] 
-
-employeeCitiesHash2 = {  
-  'Racel'=>'SF',
-  'brian'=>'Oakland',
+employeeCitiesHash2 = {
+  'racel'=>'SF',
+  'Brian'=>'Oakland',
   'Sarah'=>'Berkely',
   'Joseph'=>'Palo Alto',
-  }
-puts "\n*** Keys common to both hashes   ***"  
+                        }
+puts "\n3*** .keys common to both hashes   ***"
 puts employeeCitiesHash1.keys & employeeCitiesHash2.keys  
 
-puts "\n*** Values common to both Hashes***"
+puts "\n4*** .values common to both Hashes***"
 puts employeeCitiesHash1.values & employeeCitiesHash2.values 
 
-puts "\n*** Values from first hash that are not in second ***" 
-puts  employeeCitiesHash1.values - employeeCitiesHash2.values 
+puts "\n5*** Values from first hash that are not in second ***"
+puts  employeeCitiesHash1.values - employeeCitiesHash2.values
 
+puts "\n5.1*** Values from first hash that are not in second ***"
+puts  employeeCitiesHash2.values - employeeCitiesHash1.values
+
+puts "\n6*** Values from first hash and second ***"
+puts  employeeCitiesHash1.values + employeeCitiesHash2.values
