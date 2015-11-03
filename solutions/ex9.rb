e1052f6d4716d81c08acd388914178bd5eba2d40
phuong hua <businessph>
2015-11-03 15:02:16 -0800

    11032015 1501 added description

--- solutions/ex9.rb
+++ solutions/ex9.rb
@@ -1,31 +1,33 @@
-puts require_relative( "ex9_external_module.rb" ) 
+puts require_relative( "ex9_external_module.rb" )
+
+###include module CalorieMeasurer
 include CalorieMeasurer 
 
+### create set and get methods for the following
 class Breakfast 
-  attr_accessor :dish, :calories, :price
+  attr_accessor :dish, :price, :calories
 end
 
-
+### creates new instance of class
 breakfast = Breakfast.new
 
-
+### print to get, class instance to set, which equals get
 print "\nEnter dish: "
 breakfast.dish = gets.chomp
 
-print "Enter price: "
+### print to get, class instance to set, which equals get
+print "\nEnter price: "
 breakfast.price = gets.chomp.to_f
 
-
-print "Enter calories: "
+### print to get, class instance to set, which equals get
+print "\nEnter calories: "
 breakfast.calories = gets.chomp.to_i
 
-
-
+###
 puts "Breakfast of an #{breakfast.dish} cost $#{breakfast.price.round(2)}
 and I gained #{breakfast.calories} calories 
 at the cost of $#{(breakfast.price/breakfast.calories).round(2)} per calorie. 
-#{measureCalories(breakfast.calories)} for me."
-
+#{measureCalories(breakfast.calories)} for me." ### module method
 
 #The IDE is having syntax-highlighting problems, the Ruby interpreter knows better
 
--- solutions/ex9_external_module.rb
+++ solutions/ex9_external_module.rb
@@ -5,7 +5,7 @@ module CalorieMeasurer
     UN_HEALTHY = "too many calories"
    
     def measureCalories(calories) 
-       if calories < 150 then
+        if calories < 150 then
           return "That is #{HEALTHY}"
         else       
           return "That is #{UN_HEALTHY}"
