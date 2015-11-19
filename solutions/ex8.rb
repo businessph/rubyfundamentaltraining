7dafce5a5778dcf79e0344d1b2d6f0a693af25fc
phuong hua <businessph>
2015-11-18 16:14:44 -0800

    11182015 1614

--- solutions/ex8.rb
+++ solutions/ex8.rb
@@ -1,20 +1,41 @@
+#
+# print "In which city do you live?"
+# residentOfCity = gets.chomp
+#
+# print "\nIn which city do you work?"
+# workCity = gets.chomp
+#
+# print "\nHow far (in miles) is your work from your home?"
+# miles = gets.chomp.to_i
+#
+#   if residentOfCity == workCity
+#     puts "lucky"
+#   elsif (residentOfCity != workCity) and miles < 5
+#     puts "quite lucky"
+#   elsif (residentOfCity != workCity) and miles < 10
+#     puts "still quite lucky"
+#   else
+#     puts "\noh well, what to do?"
+#   end
 
-print "In which city do you live?" 
-residentOfCity = gets.chomp
+####################################################################
 
-print "\nIn which city do you work?" 
-workCity = gets.chomp
+print "city live in?"
+city_live_in = gets.chomp
 
-print "\nHow far (in miles) is your work from your home?" 
-miles = gets.chomp.to_i
+print "\ncity work in?"
+city_work_in = gets.chomp
 
-  if residentOfCity == workCity 
-    puts "lucky" 
-  elsif (residentOfCity != workCity) and miles < 5
-    puts "quite lucky" 
-  elsif (residentOfCity != workCity) and miles < 10
-    puts "still quite lucky" 
-  else
-    puts "\noh well, what to do?" 
-  end 
+print "\ncommute miles?"
+commute_miles = gets.chomp.to_i
+
+if city_live_in == city_work_in
+  puts "same city, lucky"
+elsif (city_live_in != city_work_in) and commute_miles < 5
+  puts "short commute, quite lucky"
+elsif (city_live_in != city_work_in) and commute_miles < 10
+  puts "long commute, still quite lucky"
+else
+  puts "\nfar commute, oh well, what to do?"
+end
 
