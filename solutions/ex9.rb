51e8f4715a4fdf3a5af0835a9786d2f715e8e43a
phuong hua <businessph>
2015-11-19 20:49:27 -0800

    11192015 2049 update ex9

--- solutions/ex9.rb
+++ solutions/ex9.rb
@@ -1,14 +1,16 @@
-puts require_relative( "ex9_external_module.rb" )
 
-###include module CalorieMeasurer from external file
+### -main file needs an external file.  external file needs to be in the same directory/folder as main
+require_relative( "ex9_external_module.rb" )
+
+### -main file needs external module
 include CalorieMeasurer 
 
-### create set and get methods for the following
+### -create set and get methods for the following
 class Breakfast 
-  attr_accessor :dish, :price, :calories
+  attr_accessor :dish, :price, :calories, :service, :review
 end
 
-### creates new instance of class
+### -creates new instance of class
 breakfast = Breakfast.new
 
 ### print to get, class instance to set, which equals get
@@ -23,11 +25,18 @@ breakfast.price = gets.chomp.to_f
 print "\nEnter calories: "
 breakfast.calories = gets.chomp.to_i
 
+print "\nhow was the service"
+breakfast.service = gets.chomp
+
+print "\nwhat is your overall experience"
+breakfast.review = gets.chomp
+
 ###
-puts "Breakfast of an #{breakfast.dish} cost $#{breakfast.price.round(2)}
+puts "\nBreakfast of an #{breakfast.dish} cost $#{breakfast.price.round(2)}
 and I gained #{breakfast.calories} calories 
 at the cost of $#{(breakfast.price/breakfast.calories).round(2)} per calorie. 
 #{measureCalories(breakfast.calories)} for me." ### module method, pass in breakfast.calories to module method
 
+puts "\nThe service was #{breakfast.service}. My review for this place is #{breakfast.review}."
 #The IDE is having syntax-highlighting problems, the Ruby interpreter knows better
 
