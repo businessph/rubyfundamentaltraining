0d06e3e5df9c10e8795b5ab1790939cdf97cab59
phuong hua <businessph>
2015-11-23 14:14:47 -0800

    11232015 1414 update ex 10

--- solutions/exa10.rb
+++ solutions/exa10.rb
@@ -11,31 +11,32 @@ $phonebookArr = Array.new()
 $phonebookArr[0] = ["Ruven","415..."]
 $phonebookArr[1] = ["David","510..."]
 $phonebookArr[2] = ["Barbara","604..."]
-$phonebookArr[3] = ["Barbara1","604..."]
+$phonebookArr[3] = ["Barbara1","604.1..."]
+$phonebookArr[4] = ["Barbara2","604.2..."]
 
 ### saves data to disk in YAML format
 ### the to_yaml method converts to YAML format
 def saveDB      
-    File.open($phoneBookyml,'w') {             ### open file > write
+    File.open($phoneBookyml,'w') {             ### open yml file > write
         |file|                                 ### loop
-         file.write($phonebookArr.to_yaml)     ### take the file, write, to yml
-                         }
+         file.write($phonebookArr.to_yaml)     ### take the file, write, Array.new
+                                   }
 end
 
 def loadDB
-    input_data = File.read($phoneBookyml)      ### read file, array
+    input_data = File.read($phoneBookyml)      ### read yml file, array
     $phonebookArr = YAML::load(input_data)     ### created array equals load
 end
 
 def showData
-    puts $phonebookArr.to_yaml                 ### puts array to yml
+    puts $phonebookArr.to_yaml                 ### create puts Array.new to yml
 end
 
-if File.exist?($phoneBookyml) then             ### if file exists them do the following
-    saveDB
-    loadDB
-    showData
-else
+if File.exist?($phoneBookyml) then             ### if file exists save, load, and show
+    saveDB                                     ### 1
+    loadDB                                     ### 2
+    showData                                   ### 3
+  else
     puts "The file #{$phoneBookyml} cannot be found."    ### else put he following statement
 end
 
